26dbd299302ba816efec86ddf5a67a966307b912
Volan-de-krot <115557216+kv-zayceva@users.noreply.github.com>
2023-03-30 17:33:55 +0300

    PetrolCalculatorClasses.java
    Vnesla esp

--- src/PetrolCalculatorClasses.java
+++ src/PetrolCalculatorClasses.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class PetrolCalculatorClasses {
     public void calculate() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of actions => ");
+        System.out.print("Ingresa el número de acciones => ");
         int iterationsCount = scanner.nextInt();
         for (int counter = 0; counter < iterationsCount; counter++) {
-            System.out.print("Enter fuel type: 1 - 92 octane gas, 2 - 95 octane gas => ");
+            System.out.print("Ingresa el tipo de combustible: 1 — gasolina de 92 octanos, 2 — gasolina de 95 => ");
             int petrolType = scanner.nextInt();
-            System.out.print("Enter the number of volume => ");
+            System.out.print("Ingresa el número de volumen => ");
             double volume = scanner.nextDouble();
             if (petrolType == 1) {
                 double price = new Petrol1Calculator().calculate(volume);
-                System.out.println("The price of " + volume + " volume of the 92 octane gas is $" + price + );
+                System.out.println("El costo de " + volume + " litros de gasolina de 92 octanos es $" + price);
             } else if (petrolType == 2) {
                 double price = new Petrol2Calculator().calculate(volume);
-                System.out.println("The price of " + volume + " volume of the 95 octane gas is $" + price + );
+                System.out.println("El costo de " + volume + " litros de gasolina de 95 octanos es $" + price);
             }
         }
     }
